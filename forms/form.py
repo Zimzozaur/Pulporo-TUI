5565c12cbb2df4fd71a2fa900f622d42dfa95fbc
bmo <szymonpiechutowski@gmail.com>
2024-05-21 17:43:41 +0200

    Refactored code and added 2 methods: - Make JSON out of form - Check is form correctly field

--- forms/form.py
+++ forms/form.py
@@ -1,15 +1,15 @@
+import json
 from datetime import datetime
 
 from textual.app import ComposeResult
 from textual.containers import Horizontal
 from textual.widgets import Checkbox, Button, Static
 
-
 from .custom_validators import (
     TitleMax50Validator,
     DateValidator
 )
-from fields.input import NotBlinkingInput, NotBlinkingTextArea
+from fields.fields import NotBlinkingInput, NotBlinkingTextArea
 
 
 class OutflowsForm(Static):
@@ -29,25 +29,55 @@ class OutflowsForm(Static):
     }
     """
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields = {
+            'title': NotBlinkingInput(
+                id='form-title', placeholder='Title',
+                validators=[TitleMax50Validator()]
+            ),
+
+            'value': NotBlinkingInput(
+                id='form-value', placeholder='Value',
+                type='number',
+                restrict=r'^(0|[1-9]\d{0,14})(\.\d{0,2})?$',
+            ),
+            'date': NotBlinkingInput(
+                id='form-date', placeholder='Date DD-MM-YYYY',
+                type='text', value=datetime.today().strftime('%-d-%-m-%Y'),
+                restrict=r'^\d{0,2}-?\d{0,2}-?\d{0,4}$',
+                validators=[DateValidator()],
+            ),
+            'prediction': Checkbox('Prediction', True, id='form-prediction'),
+            'notes': NotBlinkingTextArea(id='form-textarea', show_line_numbers=True),
+        }
+
     def compose(self) -> ComposeResult:
-        yield NotBlinkingInput(
-            id='form-title', placeholder='Title',
-            validators=[TitleMax50Validator()]
-        )
-        yield NotBlinkingInput(
-            id='form-value', placeholder='Value',
-            type='number',
-            restrict=r'^(0|[1-9]\d{0,14})(\.\d{0,2})?$',
-        )
-        yield NotBlinkingInput(
-            id='form-date', placeholder='Date DD-MM-YYYY',
-            type='text', value=datetime.today().strftime('%-d-%-m-%Y'),
-            restrict=r'^\d{0,2}-?\d{0,2}-?\d{0,4}$',
-            validators=[DateValidator()],
-        )
-        yield Checkbox('Prediction', True, id='form-prediction')
-        yield NotBlinkingTextArea(id='form-textarea', show_line_numbers=True)
+        yield self.fields['title']
+        yield self.fields['value']
+        yield self.fields['date']
+        yield self.fields['prediction']
+        yield self.fields['notes']
         with Horizontal(id='form-action-buttons'):
             yield Button('Cancel', variant='error', id='form-cancel-button')
             yield Static()
             yield Button('Submit', variant='success', id='form-submit-button')
+
+    def form_valid(self):
+        """Return is form valid """
+        for field in self.fields.values():
+            if not isinstance(self.fields, NotBlinkingTextArea) or not field.is_valid:
+                return False
+        return True
+
+    def form_to_json(self):
+        """Return JSON representation of a form"""
+        form_data = {
+            'title': self.fields['title'].value,
+            'value': self.fields['value'].value,
+            'date': self.fields['date'].value,
+            'prediction': self.fields['prediction'].value,
+            'notes': self.fields['notes'].text,
+        }
+        return json.dumps(form_data)
+
