5467b56735c953a9d256ee923246588f58b16244
bmo <szymonpiechutowski@gmail.com>
2024-05-31 14:39:43 +0200

    Simplified popup and when user click cancel or submit form is removed and list of forms is displayed

--- screens/create_new.py
+++ screens/create_new.py
@@ -38,11 +38,6 @@ class CreateNewPopup(ModalScreen):
         width: 100%;
     }
     
-    #show-forms {
-        margin-top: 1;
-        width: 70%;
-    }
-    
     #list-form-wrapper {
         margin-top: 1;
         width: 90%;
@@ -58,8 +53,6 @@ class CreateNewPopup(ModalScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.api = OneOffClient()
-        self.toggle_list = False
-        self.toggle_form = False
         self.current_form = None
         self.options = [
             Option('Outflow One-off', id='outflow-one-off'),
@@ -72,43 +65,24 @@ class CreateNewPopup(ModalScreen):
             with Center():
                 yield Label("CREATE NEW", id='header-title')
             with Center():
-                yield Button('Show forms', id='show-forms')
-            with Center():
                 with VerticalScroll(id='list-form-wrapper'):
-                    pass
+                    yield OptionList(*self.options, id="choose-form")
 
     def on_click(self, event: Click):
         """Close popup when clicked on the background"""
         if self.get_widget_at(event.screen_x, event.screen_y)[0] is self:
             self.dismiss(False)
 
-    @on(Button.Pressed, '#show-forms')
-    def toggle_list_display(self) -> None:
-        """Toggle the visibility of the form list when the button is pressed"""
-        self.toggle_list = not self.toggle_list
-
-        if self.toggle_form:
-            self.toggle_form = False
-            self.query_one('#popup-form').remove()
-
-        if self.toggle_list:
-            self.query_one('#show-forms').variant = 'primary'
-            forms_list = self.query_one("#list-form-wrapper")
-            forms_list.mount(OptionList(*self.options, id="choose-form"))
-        else:
-            self.query_one('#show-forms').variant = 'default'
-            self.query_one('#choose-form').remove()
-
     @on(Button.Pressed, '#form-cancel-button')
     def remove_form_from_dom(self) -> None:
         """Remove form from dom on cancel button click"""
+        self.current_form = None
         self.query_one('#popup-form').remove()
-        self.query_one('#show-forms').disabled = False
-        self.toggle_form = False
-        self.toggle_list_display()
+        option_list = OptionList(*self.options, id="choose-form")
+        self.query_one('#list-form-wrapper').mount(option_list)
 
     @on(Button.Pressed, '#form-submit-button')
-    def remove_form_from_dom(self) -> None:
+    def send_request(self) -> None:
         """Send request and remove form from DOM when accepted"""
         match self.current_form:
             case 'outflow-one-off':
@@ -118,24 +92,19 @@ class CreateNewPopup(ModalScreen):
                 form = self.query_one(InflowsForm)
                 self.api.post_flow('inflows/', form.form_to_dict())
 
-        self.current_form = None
+        self.remove_form_from_dom()
 
     def on_option_list_option_selected(self, event: OptionList.OptionSelected) -> None:
         """Called when Option is clicked"""
-
-        self.toggle_list = False
-        self.toggle_form = True
         selected_option = event.option.id
-        show_form_bt = self.query_one('#show-forms')
-        show_form_bt.variant = 'default'
-        show_form_bt.disabled = True
         self.query_one('#choose-form').remove()
 
         match selected_option:
             case 'outflow-one-off':
-                self.current_form = 'outflow-one-off'
                 self.query_one('#list-form-wrapper').mount(OutflowsForm(id='popup-form'))
             case 'inflow-one-off':
-                self.current_form = 'inflow-one-off'
                 self.query_one('#list-form-wrapper').mount(InflowsForm(id='popup-form'))
+
+        self.current_form = selected_option
+
 
