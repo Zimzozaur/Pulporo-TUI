7948fdc5a263aadc8972699fcd9283f22df0a9e2
bmo <szymonpiechutowski@gmail.com>
2024-06-19 10:52:27 +0200

    Feature: - Added forms to __init__.py for easier imports

---
+++ forms/__init__.py
@@ -0,0 +1,11 @@
+"""Forms provides a comprehensive set of forms for various tasks within the application."""
+
+
+# Local imports.
+from .forms import InflowsForm, OutflowsForm
+
+
+# Public symbols.
+__all__ = [
+    OutflowsForm, InflowsForm
+]
